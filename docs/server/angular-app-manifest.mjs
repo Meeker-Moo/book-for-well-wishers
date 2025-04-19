c6fb9ab6ea3ff2b259831bd7ee23862932aec60a
Meeker <005527@cdg.co.th>
2025-04-19 22:38:54 +0700

    init code

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -29,11 +29,11 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 524, hash: 'fedc76b3175b9920347f63e002dd94736ccab41e2fa96fe02d116cb89aa02e32', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1037, hash: '687af03cb5b9b4fdd65feca927b63014bf5e8bfc28a9e4b039d1b781331c9128', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'promptpay/index.html': {size: 20966, hash: 'b8c48e5982f5f37aa3084fa1f7b67bc6d56fa5244762abdf258a386460df9da0', text: () => import('./assets-chunks/promptpay_index_html.mjs').then(m => m.default)},
-    'list-wishers/index.html': {size: 20976, hash: 'e83d47888d9529f695d7e656d56490307d05390c56085826c51645d538f09ef6', text: () => import('./assets-chunks/list-wishers_index_html.mjs').then(m => m.default)},
-    'wishers/index.html': {size: 20961, hash: 'f13c424daef44a9c9a994ba0562b84b92c01a92c94c6365dbfc050c0cf2bbd68', text: () => import('./assets-chunks/wishers_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 524, hash: '3ee0bf5a983724b1064f2558d4b9d4268cfd14e16e26f4b816542f6b034545af', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1037, hash: '20dbb28088b8c42728eac79a3fc91fbc174b608e707ee20d57e9340f79be9400', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'list-wishers/index.html': {size: 20982, hash: 'a946da9ad0d6d9e0a3d161de2177d41850e4a377fe1c058b9f5038c7931f907b', text: () => import('./assets-chunks/list-wishers_index_html.mjs').then(m => m.default)},
+    'promptpay/index.html': {size: 20982, hash: 'a946da9ad0d6d9e0a3d161de2177d41850e4a377fe1c058b9f5038c7931f907b', text: () => import('./assets-chunks/promptpay_index_html.mjs').then(m => m.default)},
+    'wishers/index.html': {size: 20982, hash: 'a946da9ad0d6d9e0a3d161de2177d41850e4a377fe1c058b9f5038c7931f907b', text: () => import('./assets-chunks/wishers_index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
