65c29236f657c978cf9c25f38ea490161cc3f036
Meeker <005527@cdg.co.th>
2025-04-21 08:10:03 +0700

    init code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,17 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { WishersComponent } from './wishers/wishers.component';
+import { SwitcherComponent } from './switcher/switcher.component';
 import { ListWishersComponent } from './list-wishers/list-wishers.component';
 import { PromptpayComponent } from './promptpay/promptpay.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ListPromptpayComponent } from './list-promptpay/list-promptpay.component';
 
 export const routes: Routes = [
+    { path: '', component: SwitcherComponent },
     { path: 'wishers', component: WishersComponent },
     { path: 'list-wishers', component: ListWishersComponent },
     { path: 'promptpay', component: PromptpayComponent },
-    {path: '', redirectTo: 'wishers', pathMatch: 'full'},
+    { path: 'list-promptpay', component: ListPromptpayComponent },
+    { path: '', redirectTo: '/', pathMatch: 'full' },
     { path: '**', component: PageNotFoundComponent }
 ];
---
+++ src/app/switcher/switcher.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-switcher',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './switcher.component.html',
+  styleUrl: './switcher.component.scss'
+})
+export class SwitcherComponent {
+
+}
